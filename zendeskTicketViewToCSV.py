dd74b4eb0f815af240d240783c0156d268977ccd
Senoshibe <k_m_choi@outlook.com>
2025-01-26 21:24:59 +1100

    26/01/2025

--- zendeskTicketViewToCSV.py
+++ zendeskTicketViewToCSV.py
@@ -15,7 +15,7 @@ auth = f'{ZENDESK_USER_EMAIL}/token', ZENDESK_API_TOKEN
 print(f'Getting tickets from view ID {view_id}')
 url = f'{ZENDESK_SUBDOMAIN}/api/v2/views/{view_id}/tickets.json'
 while url:
-    response = requests.get(url, auth=auth)
+    response = requests.get(url, auth=auth) 
     if response.status_code == 200:
         page_data = response.json()
         tickets = page_data['tickets']
@@ -24,3 +24,24 @@ while url:
     else:
         print(f"Failed to retrieve tickets: {response.reason}")
         url = None  # Exit loop on failure
+
+rows = [('Ticket ID', 'Requester', 'Assignee', 'Status', 'Subject', 'Description', 'Last Updated', 'Ticket URL')]
+
+for ticket in view_tickets:
+    row = (
+        ticket['id'],
+        ticket['requester'],
+        ticket['assignee_email'],
+        ticket['status'],
+        ticket['subject'],
+        ticket['description'],
+        ticket['updated_at'],
+        ticket['url'],
+        f'https://{ZENDESK_SUBDOMAIN}.zendesk.com/agent/tickets/{ticket["id"]}'
+    )
+    rows.append(row)
+
+with open('crowdstrike_tickets.csv', mode='w', newline='') as csv_file:
+   report_writer = csv.writer(csv_file, dialect='excel')
+   for row in rows:
+       report_writer.writerow(row)
